ddeb2572cea7c7451cf02f260fd6305b141d2f13
Michele Bisignano <bisignano.dev@gmail.com>
2025-12-22 23:39:29 +0100

    Update target ADC value and safety parameters in configuration files

--- Firmware/include/Config.h
+++ Firmware/include/Config.h
@@ -20,9 +20,9 @@ namespace Config {
         constexpr unsigned long TELEMETRY_MS = 50;
 
         // Raw ADC Setpoints
-        constexpr float TARGET_ADC = 2053;
+        constexpr float TARGET_ADC = 2153;
         constexpr float IDLE_THRESHOLD = 54;
-        constexpr float SAFETY_MIN_ADC = 700;
+        constexpr float SAFETY_MIN_ADC = 1726;
         constexpr float SAFETY_MAX_ADC = 2806;
         
         // Safety Constants
